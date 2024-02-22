88e66622ad2437df5be6ea1c4878fdddf7ae6e5c
Arturo Mora-Rioja <arturomora@gmail.com>
2024-02-22 09:15:14 +0100

    User-friendly input validation for the phone textbox added

--- js/contactus.js
+++ js/contactus.js
@@ -2,4 +2,9 @@
 
 document.querySelector('#frmContact').addEventListener('submit', function(e) {
     e.preventDefault();
+
+    if (!/[0-9]{8}/.test(document.querySelector('#txtPhone').value)) {
+        document.querySelector('span[data-input=txtPhone]').classList.remove('hidden');
+        return false;
+    }
 });
